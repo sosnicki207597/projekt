21d58ef43b1f25dd2cfe174d004435426101e0c7
Michał Sośnicki <sosnicki.michal@gmail.com>
2017-02-28 15:36:19 +0100

    Add maven

---
+++ src/main/java/in/evils/majkel/App.java
@@ -0,0 +1,13 @@
+package in.evils.majkel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
