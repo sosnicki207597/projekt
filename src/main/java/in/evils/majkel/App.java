6ef3169b59af88fb411a722366263cdfdcab6c89
lcyran <207537@edu.p.lodz.pl>
2017-03-07 15:18:48 +0100

    test function

--- src/main/java/in/evils/majkel/App.java
+++ src/main/java/in/evils/majkel/App.java
@@ -2,12 +2,13 @@ package in.evils.majkel;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(test());
+    }
+
+    private static String test() {
+        return "D\\/P/\\";
     }
 }
